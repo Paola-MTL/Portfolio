8b38a2ed7823294283e245b1eb0c409e5fe0c5fa
Paola <cejoco.paola@gmail.com>
2026-08-25 13:31:26 +0200

    Remove ProjectsGrid and ContactCTA from the homepage
    The new hero is the whole homepage now; those sections still exist as
    components and are used from the project and about pages.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,5 @@
 import Hero from "@/components/Hero";
-import ProjectsGrid from "@/components/ProjectsGrid";
-import ContactCTA from "@/components/ContactCTA";
 
 export default function Home() {
-  return (
-    <>
-      <Hero />
-      <ProjectsGrid />
-      <ContactCTA />
-    </>
-  );
+  return <Hero />;
 }
